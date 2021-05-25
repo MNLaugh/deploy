503a98fe59074f7809c74d289815c45f91ac0b3a
MNLaugh <contact@mnlaugh.com>
2021-05-25 03:01:39 +0200

    test

--- push.sh
+++ push.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-$v = "1.0.3"
-$a = "v$v"
-
+v = "1.0.3"
+a = "v$v"
 
 git add .
 git commit -m "test"
-git tag -a "$a" -m "Version $v"
-git push origin "$a"
+git tag -a $a -m "Version $v"
+git push origin $a
