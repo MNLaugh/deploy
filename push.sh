f8e82df2a439d9a338761eb87aedae25bcf1aac1
MNLaugh <contact@mnlaugh.com>
2021-05-26 11:37:36 +0200

    com

--- push.sh
+++ push.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 m="commit description"
 
+# global push in main
 git add .
 git commit -m "$m"
 git push origin main
 
+# create new tag if an argument is set
 if [[ $1 ]]; then
   v=$1
   a="v$v"
